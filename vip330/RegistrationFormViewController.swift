1ea461efe8bfc92ac4e2ff9f11d3f592a86ffc92
Ivan yavorin <ivanyavorin@mac-mini-ivan.dlink>
2016-01-18 20:59:06 +0200

    initial commit.

---
+++ vip330/RegistrationFormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegistrationFormViewController.swift
+//  vip330
+//
+//  Created by Ivan Yavorin on 1/18/16.
+//  Copyright © 2016 Massmaker. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
